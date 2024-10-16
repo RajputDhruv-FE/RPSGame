c1493510c7d1099812c1754b4af76b034e3f0af4
RajputDhruv-FE <rajputdhruv443@gmail.com>
2024-10-16 18:06:59 +0530

    login page developed to the end

---
+++ Test Files/Lobby/login/login.js
@@ -0,0 +1,73 @@
+const emojis = document.querySelectorAll(".emoji");
+const container = document.querySelector(".emoji-container");
+const containerWidth = container.clientWidth;
+const containerHeight = container.clientHeight;
+
+emojis.forEach((emoji) => {
+  let posX = Math.random() * (containerWidth - 50); // Adjusting for emoji size
+  let posY = Math.random() * (containerHeight - 50); // Adjusting for emoji size
+  emoji.style.left = `${posX}px`;
+  emoji.style.top = `${posY}px`;
+
+  let speedX = (Math.random() * 2 + 1) * (Math.random() > 0.5 ? 1 : -1);
+  let speedY = (Math.random() * 4 + 1) * (Math.random() > 0.5 ? 1 : -1);
+
+  function moveEmoji() {
+    posX += speedX;
+    posY += speedY;
+
+    if (posX <= -100 || posX + 100 >= containerWidth) {
+      speedX = -speedX; // Reverse direction on X-axis
+    }
+
+    if (posY <= -100 || posY + 100 >= containerHeight) {
+      speedY = -speedY; // Reverse direction on Y-axis
+    }
+
+    emoji.style.left = `${posX}px`;
+    emoji.style.top = `${posY}px`;
+
+    requestAnimationFrame(moveEmoji);
+  }
+
+  moveEmoji();
+});
+
+
+      // Password field logic
+      const passwordInput = document.getElementById("password");
+      const passwordIcon = document.getElementById("passwordIcon");
+   
+
+      passwordInput.addEventListener("input", function () {
+        if (passwordInput.value === "") {
+            passwordIcon.classList.remove("ri-eye-close-fill");
+            passwordIcon.classList.add("ri-key-fill");
+       
+        } else {
+            passwordIcon.classList.remove("ri-key-fill");
+            passwordIcon.classList.add("ri-eye-close-fill");
+        }
+      });
+
+      passwordIcon.addEventListener("click", function () {
+        const type =
+          passwordInput.getAttribute("type") === "password"
+            ? "text"
+            : "password";
+        passwordInput.setAttribute("type", type);
+
+        // Toggle between eye close and eye open icons
+        passwordIcon.classList.remove("ri-key-fill",'ri-eye-close-fill');
+            passwordIcon.classList.add("ri-eye-fill");
+        
+       if(type === "password"){
+        passwordIcon.classList.remove("ri-key-fill","ri-eye-fill");
+        passwordIcon.classList.add("ri-eye-close-fill");
+       }
+       else{
+        passwordIcon.classList.remove("ri-key-fill","ri-eye-close-fill");
+        passwordIcon.classList.add("ri-eye-fill");
+       }
+      });
+
